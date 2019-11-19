f3066abc7e1db77d686c2134df14ea1b3c69690c
Filip Todoroski <ftodoroski@ymail.com>
2019-11-19 17:03:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,8 +3,26 @@ require 'directors_database'
 require 'pp'
 
 
-
 def directors_totals(nds)
-  
+    result = {}
 
+    i = 0
+    while i < nds.length
+        director_movies = nds[i][:movies]
+        director_name = nds[i][:name]
+        gross_total = 0
+
+        j = 0
+        while j < director_movies.length
+            worldwide_gross = director_movies[j][:worldwide_gross]
+            gross_total += worldwide_gross
+
+            j += 1
+        end
+        result[director_name] = gross_total
+
+        i += 1
+    end
+
+    result
 end
